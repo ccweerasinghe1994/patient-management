d9455a36e5b31212e1e1c33802c8622bc0416a1a
wchamara <chamara.weerasinghe@my.sliit.lk>
2025-03-19 06:28:54 +0530

    initialize patiemt-service project with basic structure and dependencies

---
+++ patiemt-service/src/main/java/com/cgnexus/patiemtservice/PatiemtServiceApplication.java
@@ -0,0 +1,13 @@
+package com.cgnexus.patiemtservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PatiemtServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PatiemtServiceApplication.class, args);
+	}
+
+}
---
+++ patiemt-service/src/test/java/com/cgnexus/patiemtservice/PatiemtServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cgnexus.patiemtservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PatiemtServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
